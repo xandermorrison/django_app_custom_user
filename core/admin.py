e691ab9075507044907a45d55a1bcbd488793067
Xander Morrison <dark_knight@zandanokonpyuta.local>
2020-02-05 17:29:27 -0600

    Start

---
+++ core/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext, gettext_lazy as _
+
+from .models import User
+from django.contrib.auth.models import Group
+
+class MyAdminSite(admin.AdminSite):
+    site_header = "Matter Admin"
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+    search_fields = ('first_name', 'last_name', 'email')
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    ordering = ('last_name', 'first_name')
+
+admin_site = MyAdminSite(name="admin")
+
+# Register your models here.
+admin_site.register(User, CustomUserAdmin)
+admin_site.register(Group)
---
+++ core/models.py
@@ -0,0 +1,75 @@
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, UserManager, PermissionsMixin
+
+# Create your models here.
+
+class CustomUserManager(UserManager):
+
+    def _create_user(self, email, password, **extra_fields):
+        
+        if not email:
+            raise ValueError("The email must be set.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(_("email address"), blank=False, unique=True)
+    first_name = models.CharField(_("first name"), max_length=30, blank=False)
+    last_name = models.CharField(_("last name"), max_length=150, blank=False)
+
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        """Return the short name for the user."""
+        return self.first_name
